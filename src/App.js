2f5a249cde20f2f07428a9603c6d6087e5947b22
RanjeetUttamPatil <ranjeetpatil01012006@gmail.com>
2025-12-24 14:13:19 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import CitySelector from './pages/CitySelector';
+import Dashboard from './pages/Dashboard';
+import TreeImpact from './pages/TreeImpact';
+import Wildlife from './pages/Wildlife';
+import Chatbot from './pages/Chatbot';
+import GreenSuggestionsPage from './pages/GreenSuggestionsPage';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/city-selector" element={<CitySelector />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tree-impact" element={<TreeImpact />} />
+          <Route path="/wildlife" element={<Wildlife />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/green-suggestions" element={<GreenSuggestionsPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
